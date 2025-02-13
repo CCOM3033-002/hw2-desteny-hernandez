a8697f755059e042bea6cfb0ae48c145d7d42f5c
Desteny Hernandez <desteny.hernandez1@upr.edu>
2025-02-13 15:34:34 +0000

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,38 @@
-// escriba su nombre, username en github y numero de estudiante en el header
-// incluya cualquier colaboración que haga
-#include<iostream>
+/*
+Asignación 2: Suma de dos fracciones
+Desteny Hernandez De Juan
+github: desteny-hernandez
+401-23-3159
+*/
+
+#include <iostream>
+#include <string>
+#include <iomanip>
 using namespace std;
 
-int main(){
-  // escriba su código aquí 
-  
+// El propósito de este programa es poder hacer una suma de fracciones y tener el resultado en una fraccion.
+
+int main()
+{
+  int num1, den1, num2, den2, numSum, denMul;
+  cout << "Este programa suma dos fracciones." << endl;
+
+  cout << "Entre el numerador de la 1ra fraccion: ";
+  cin >> num1;
+
+  cout << "Entre el denominador de la 1ra fraccion: ";
+  cin >> den1;
+
+  cout << "Entre el numerador de la 2da fraccion: ";
+  cin >> num2;
+
+  cout << "Entre el denominador de la 1ra fraccion: ";
+  cin >> den2;
+
+  numSum = (num1 * den2) + (num2 * den1);
+  denMul = den1 * den2;
+
+  cout << "La suma es " << numSum << "/" << denMul << "." << endl;
+
   return 0;
 }
